492dd4278804f0b7149946264e90f8881ef9a1fe
Rafael Feitosa <rafaelfeit@hotmail.com>
2025-02-22 22:33:03 -0300

    inclusão de código para exemplo no projeto tipos de váriaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,11 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // Variáveis e constantes
+        int numero = 5;
+        numero = 10;
+
+        // Constante precisa incluir o "final" no começo e a palavra deve ser toda em maiúscula
+        final double VALOR_PI = 3.14;
     }
 }
