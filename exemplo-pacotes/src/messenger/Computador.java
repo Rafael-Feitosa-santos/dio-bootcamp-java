5120cf3577449e81dba40aa26d1ba59ccd0f60c9
Rafael Feitosa <rafaelfeit@hotmail.com>
2025-03-07 19:56:16 -0300

    criação do package messenger, aonde foi aplicado os conceitos de encapsulamento.

---
+++ exemplo-pacotes/src/messenger/Computador.java
@@ -0,0 +1,11 @@
+package messenger;
+
+public class Computador {
+    public static void main(String[] args) {
+        //abrindo MSN Messenger
+        MSNMessenger msn = new MSNMessenger();
+
+        msn.enviarMensagem();
+        msn.receberMensagem();
+    }
+}
